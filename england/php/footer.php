42d50556ec781b29794f0d2f8a25a2338c84a395
Tyler-SMI <tsanguinette@gmail.com>
2019-08-01 08:41:51 -0500

    Updated England PHP to HTML

--- england/php/footer.php
+++
@@ -1,6 +0,0 @@
-    <div class="blog-footer">
-      <p class="col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">&copy;2014. Designed and coded by Tyler Sanguinette. </p>
-      <p class="col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
-        <a href="#">Back to top</a>
-      </p>
-    </div>
--- england/php/socialmedia.php
+++
@@ -1,8 +0,0 @@
-          <div id="socialWaypoint" class="sidebar-module">
-            <h4>Social Media</h4>
-            <ol id="listfloat" class="list-unstyled">
-			  <li><a href="https://www.flickr.com/photos/tsanguinette/"><i class="fa fa-flickr fa-2x"></i></a></li>
-              <li><a href="https://twitter.com/tsanguinette/"><i class="fa fa-twitter fa-2x"></i></a></li>
-			  <li><a href="#"><i class="fa fa-youtube fa-2x"></i></a></li>
-            </ol>
-          </div>
